161ac981585254ee1e958fab80695cec09bb67f7
Ted <ted@wu.engineer>
2025-06-29 13:21:49 +0800

    Update git_config.py

--- git_config.py
+++ git_config.py
@@ -13,5 +13,5 @@ GIT_PASSWORD = 'your_github_password_or_token'
 # Format: https://github.com/username/repository.git
 GIT_REPO_URL = 'https://github.com/your_username/your_repo_name.git'
 
-# Git branch to push to (usually 'main' or 'master')
+# Git branch to push to
 GIT_BRANCH = 'main' 
