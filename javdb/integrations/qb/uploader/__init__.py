76e969d5fb426b8c9d2cd174d8e4e5aadd6ba6f4
Ted <ted@wu.engineer>
2026-05-30 15:03:58 +0800

    feat(qb): add B2 preference gate hook to upload path (ADR-022)

--- javdb/integrations/qb/uploader/__init__.py
+++ javdb/integrations/qb/uploader/__init__.py
@@ -2,6 +2,6 @@
 
 from javdb.integrations.qb.uploader.options import QbUploaderOptions
 from javdb.integrations.qb.uploader.result import QbUploaderResult
-from javdb.integrations.qb.uploader.service import run_uploader
+from javdb.integrations.qb.uploader.service import run_uploader, _preference_gate_blocks
 
-__all__ = ["QbUploaderOptions", "QbUploaderResult", "run_uploader"]
+__all__ = ["QbUploaderOptions", "QbUploaderResult", "run_uploader", "_preference_gate_blocks"]
